6663341df46dc53ccecaf1607319ac522fd6a0e7
Aditya Saxena <adityasaxena602@gmail.com>
2016-12-23 12:58:02 +0530

    https://resolutions-121.herokuapp.com/

--- submit-response.js
+++ submit-response.js
@@ -2,20 +2,17 @@ const express = require('express')
 const route = express.Router()
 
 route.post('/', function(req, res){
-		
-		console.log(req.body)
-		db.collection('chunk').save(req.body, function(err, result){
-			if(err){
-				console.log(err)
-				res.redirect('/error')
-				
-			}else{
-				console.log('data saved successfully!')
-				res.redirect('/thankyou')
-
-			}
-		})	
-	
+	console.log(req.body)
+	db.collection('chunk').save(req.body, function(err, result){
+	if(err){
+	    console.log(err)
+	    res.redirect('/error')
+	}
+	else{
+	    console.log('data saved successfully!')
+	    res.redirect('/thankyou')
+	}
+    })	
 })
 
 module.exports = route
